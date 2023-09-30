6c152f45ba34fd5b618f1298cfc3be81b82d9111
alifreytez <46208102+alifreytez@users.noreply.github.com>
2023-09-30 18:43:49 -0400

    Update statistics.js

--- scripts/statistics.js
+++ scripts/statistics.js
@@ -2,6 +2,5 @@ import Session from "./session.js"
 import DB from "./db.js";
 
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("asd")
-    //Session.pageAccess();
+    Session.pageAccess();
 });
